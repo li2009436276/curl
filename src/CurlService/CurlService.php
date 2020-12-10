8cb68678e1c0a032ecfe9b1d0f39da80f8e74dfb
li2009436276 <571816409@qq.com>
2020-12-10 22:47:27 +0800

    修改curl请求

--- src/CurlService/CurlService.php
+++ src/CurlService/CurlService.php
@@ -4,10 +4,13 @@
 namespace Curl\CurlService;
 
 
+use function Composer\Autoload\includeFile;
+
 class CurlService
 {
     static $curl = null;
-
+    static $returnType = 1; //1返回json格式，0返回原数据，
+    static $headers = [];
 
     public static function getInstance(){
 
@@ -17,18 +20,36 @@ class CurlService
         }
     }
 
+    /**
+     * curl请求
+     * @param $name
+     * @param $param  $param[0]是地址，$param[1]是数据，$param[2]是header，$param[3]是返回数据类型,$param[4]是模拟的cookie文件存放地址
+     * @return mixed
+     */
     static function __callStatic($name,$param){
 
         self::getInstance();
+
+        self::$returnType = empty($param[3]) ? 0 : 1;
+
         curl_setopt(self::$curl, CURLOPT_URL, $param[0]);
+
+        //https请求
         if(substr($param[0],0,5) == 'https'){
             curl_setopt(self::$curl, CURLOPT_SSL_VERIFYPEER, FALSE);
             curl_setopt(self::$curl, CURLOPT_SSL_VERIFYHOST, FALSE);
         }
         curl_setopt(self::$curl, CURLOPT_TIMEOUT, 10);
         curl_setopt(self::$curl, CURLOPT_RETURNTRANSFER, 1);
-        $param[2] = !empty($param[2]) ? $param[2] : ['Content-type:application/json;','Accept:application/json'];
-        curl_setopt(self::$curl,CURLOPT_HTTPHEADER,$param[2]);
+        self::$headers = !empty($param[2]) ? $param[2] : ['Content-type:application/json','Accept:application/json'];
+        curl_setopt(self::$curl,CURLOPT_HTTPHEADER,self::$headers);
+
+        //添加模拟浏览器cookie
+        if (!empty($param[4])) {
+
+            curl_setopt(self::$curl, CURLOPT_COOKIEJAR, $param[4]);
+            curl_setopt(self::$curl, CURLOPT_COOKIEFILE, $param[4]); //使用上面获取的cookies
+        }
 
         switch (strtolower($name)) {
             case 'get': return self::getReq();
@@ -53,7 +74,15 @@ class CurlService
      */
     static function postReq($data = []){
 
-        curl_setopt(self::$curl, CURLOPT_POSTFIELDS, json_encode($data));
+        if (in_array('content-type:application/x-www-form-urlencoded',self::$headers)) {
+
+            $data = http_build_query($data);
+        } else {
+
+            $data = json_encode($data);
+        }
+
+        curl_setopt(self::$curl, CURLOPT_POSTFIELDS, $data);
         return self::finish();
     }
 
@@ -64,7 +93,12 @@ class CurlService
     static function finish(){
 
         $output = curl_exec(self::$curl);
-        $output = json_decode($output,true);
+
+        if (self::$returnType) {
+
+            $output = json_decode($output,true);
+        }
+
 
         return $output;
     }
