9c0c33d11e430829216cb9cb6284a18635fa84f3
li2009436276 <571816409@qq.com>
2021-02-08 16:08:25 +0800

    添加ｕｕｉｄ

--- src/StrService/StrService.php
+++ src/StrService/StrService.php
@@ -53,4 +53,19 @@ class StrService
         }
         return $data;
     }
+
+    /**
+     * 生成唯一性的uuid
+     * @return string
+     */
+    public static function  uuid()
+    {
+        $chars = md5(uniqid(mt_rand(), true));
+        $uuid = substr ( $chars, 0, 8 ) . '-'
+            . substr ( $chars, 8, 4 ) . '-'
+            . substr ( $chars, 12, 4 ) . '-'
+            . substr ( $chars, 16, 4 ) . '-'
+            . substr ( $chars, 20, 12 );
+        return $uuid ;
+    }
 }
