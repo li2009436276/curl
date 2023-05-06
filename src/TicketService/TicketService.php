36594180324ffbb48a2eafc69b32b7e9a5b9d2bf
liwenqi <571816409@qq.com>
2023-05-06 16:55:06 +0800

    添加ticket服务

---
+++ src/TicketService/TicketService.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lmm
+ * Date: 2019/4/24
+ * Time: 18:24
+ */
+
+namespace Curl\TicketService;
+
+use Illuminate\Support\Facades\Cache;
+use MC;
+class TicketService
+{
+    /**
+     * 获取ticket
+     * 
+     */ 
+    public static function createTicket($data,$isAdmin = false){
+
+        if ($isAdmin) {
+            
+            $data['is_admin'] = 1;
+        }
+
+        $ticket = str_random(32);
+
+        Cache::put($ticket,$data,env('SESSION_LIFETIME'));
+
+        return $ticket;
+    }
+
+    /**
+     * 获取ticket的值
+     * 
+     */ 
+    public static function getTicket($ticket){
+
+        return Cache::get($ticket);
+    }
+
+    /**
+     * 删除ticket
+     * @param $ticket
+     * @return void
+     */
+    public static function deleteTicket($ticket){
+
+        Cache::forget($ticket);
+    }
+}
